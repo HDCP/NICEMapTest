bfac91ffc5e8e8e10d5c188feaa214ea50a98073
HDCP <HDCP@users.noreply.github.com>
2019-12-30 02:47:29 -0500

    Update getVehicles3.js

--- scripts/getVehicles3.js
+++ scripts/getVehicles3.js
@@ -45,7 +45,26 @@ function getVehicles3() {
               scale: 1,
               strokeWeight: 1
             }
-                
+            
+	    if (data[i].vid > 1699 && data[i].vid < 1800) {
+		  color = "#007AFB";
+		}
+		else if (data[i].vid > 1799 && data[i].vid < 1840) {
+		  color = "#AF00FB";
+		}
+		else if (data[i].vid > 1839 && data[i].vid < 1885) {	
+		  color = "#FB6E00";
+		}
+		else if (data[i].vid > 1884 && data[i].vid < 1965) {	
+		  color = "#00FBD5";
+		}
+		else if (data[i].vid > 1964 && data[i].vid < 1970) {	
+		  color = "#1300FB";
+		}
+		else {
+		  color = "#FB007E";
+		}
+	    
             function rotateMarker(selector, degree){
               // var ImgURL= window.location.origin + "/App_Themes/Default/Images/DocumentTypeIcons/RealTime-Outer.png";                  
              var ImgURL= window.location.origin + "/NICEMap/images/RealTime-Outer.png";
