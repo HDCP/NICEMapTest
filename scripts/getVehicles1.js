106cf974ec1097fac4f1fdda8cbd1765b5be8aca
HDCP <HDCP@users.noreply.github.com>
2019-12-29 22:53:11 -0500

    Update getVehicles1.js

--- scripts/getVehicles1.js
+++ scripts/getVehicles1.js
@@ -2,7 +2,8 @@ function getVehicles1() {
   // var methodName = "getrealtime";
   // var url = window.location.origin + "/NICECustomPages/getjsondata.aspx?getData=" + methodName + "&route_id="+ routeId;
   var url1 = "https://cors-anywhere.herokuapp.com/https://www.nicebus.com/NICECustomPages/getjsondata.aspx?getData=getrealtime&route_id=n19,n20G,n20H,n21,n22,n22X,n23,n24,n25,n26";
-  
+  var color = "#FB6E00";
+	
   jQuery.ajax({
        type: "GET",
        /* headers: {
