b62838edd445bd09395e0555364f12ccd05b9f36
HDCP <HDCP@users.noreply.github.com>
2019-12-30 02:31:32 -0500

    Update main.js

--- scripts/main.js
+++ scripts/main.js
@@ -4,32 +4,13 @@ var directionidValLength = directionidVal.length;
 var serviceIdValLength = serviceIdVal.length;
 var map;
 var color;
-
-if (data[i].vid > 1699 && data[i].vid < 1800) {
-  color = "#007AFB";
-}
-else if (data[i].vid > 1799 && data[i].vid < 1840) {
-  color = "#AF00FB";
-}
-else if (data[i].vid > 1839 && data[i].vid < 1885) {	
-  color = "#FB6E00";
-}
-else if (data[i].vid > 1884 && data[i].vid < 1965) {	
-  color = "#00FBD5";
-}
-else if (data[i].vid > 1964 && data[i].vid < 1970) {	
-  color = "#1300FB";
-}
-else {
-  color = "#FB007E";
-}
 	
 var markerStore1 = {};
 var markerStore2 = {};
 var markerStore3 = {};
 var markerStore4 = {};
 var markerStore5 = {};
-var markerStore = {};
+// var markerStore = {};
 var currentRouteColor = "";
 var infowindow = "";
 	
