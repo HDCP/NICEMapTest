020b8e0813b260659898c33fa9f3787158205b10
HDCP <HDCP@users.noreply.github.com>
2020-01-01 18:08:52 -0500

    Update getVehicles1.js

--- scripts/getVehicles1.js
+++ scripts/getVehicles1.js
@@ -1,8 +1,8 @@
 function getVehicles1() {
   // var methodName = "getrealtime";
   // var url = window.location.origin + "/NICECustomPages/getjsondata.aspx?getData=" + methodName + "&route_id="+ routeId;
-  var url1 = "https://cors-anywhere.herokuapp.com/https://www.nicebus.com/NICECustomPages/getjsondata.aspx?getData=getrealtime&route_id=MMCS,ElFx,n1,n4,n4X,n6,n6X,n15";
-	
+  var url1 = "https://cors-anywhere.herokuapp.com/https://www.nicebus.com/NICECustomPages/getjsondata.aspx?getData=getrealtime&route_id=MMCS,ElFx,n1,n4,n4X,n6,n6X,n15";	
+  var vehicleIDString;
   jQuery.ajax({
        type: "GET",
        url: url1,
@@ -15,6 +15,7 @@ function getVehicles1() {
        success: function (response)
        {
           var data = response.ResultData;
+	  var infowindow = new google.maps.InfoWindow({content: vehicleIDString});
           // var icon = window.location.origin + "/App_Themes/Default/Images/DocumentTypeIcons/RealTime-Inner-bus-lines.png";
           // var outer_icon = window.location.origin + "/App_Themes/Default/Images/DocumentTypeIcons/RealTime-Outer.svg";
 	  var icon = window.location.origin + "/NICEMap/images/RealTime-Inner-bus-lines.png";
@@ -37,7 +38,7 @@ function getVehicles1() {
           if(data.length > 0 && data[0].lat != "") {
            for (var i=0; i< data.length; i++) {
             point = new google.maps.LatLng({lat: parseFloat(data[i].lat), lng: parseFloat(data[i].lon)})
-            var infowindow = new google.maps.InfoWindow({content: "Vehicle #" + parseString(data[i].vid)});
+            var vehicleIDString = "Vehicle #" + data[i].vid;
             var baseIcon = {
               url: icon + '#' + data[i].vid,
               size: new google.maps.Size(42, 42),
