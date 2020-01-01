cc4d3e8924f503862684bcfd8e8da3056d6636fb
HDCP <HDCP@users.noreply.github.com>
2020-01-01 17:33:47 -0500

    Update getVehicles5.js

--- scripts/getVehicles5.js
+++ scripts/getVehicles5.js
@@ -128,9 +128,12 @@ function getVehicles5() {
 		else if (data[i].vid > 1964 && data[i].vid < 1970) {	
 		  color = "#1300FB";
 		}
-		else {
+		else if (data[i].vid > 1969 && data[i].vid < 1985) {
 		  color = "#FB007E";
-		}
+		}   
+		else {
+		  color = "#FFFB00";	
+		}   
 		    
             }
             function colorMarker(selector){
